59675e9938205ff74ea60aa922673568075c4055
Kennie Harold <kennie.harold@gmail.com>
2021-08-25 15:58:17 +0800

    Change collections to libe

--- src/firebase/collections.js
+++ src/firebase/collections.js
@@ -3,47 +3,47 @@ import {APP_ENV} from '@env';
 
 export const userCollection =
   APP_ENV === 'production'
-    ? firestore().collection('users')
+    ? firestore().collection('liveUsers')
     : firestore().collection('devUsers');
 
 export const storeCollection =
   APP_ENV === 'production'
-    ? firestore().collection('stores')
+    ? firestore().collection('liveStores')
     : firestore().collection('devStores');
 
 export const categoryCollection =
   APP_ENV === 'production'
-    ? firestore().collection('categories')
+    ? firestore().collection('liveCategories')
     : firestore().collection('devCategories');
 
 export const productCollection =
   APP_ENV === 'production'
-    ? firestore().collection('products')
+    ? firestore().collection('liveProducts')
     : firestore().collection('devProducts');
 
 export const cartCollection =
   APP_ENV === 'production'
-    ? firestore().collection('carts')
+    ? firestore().collection('liveCarts')
     : firestore().collection('devCarts');
 
 export const bannerCollection =
   APP_ENV === 'production'
-    ? firestore().collection('banners')
+    ? firestore().collection('liveBanners')
     : firestore().collection('devBanners');
 
 export const orderCollection =
   APP_ENV === 'production'
-    ? firestore().collection('orders')
+    ? firestore().collection('liveOrders')
     : firestore().collection('devOrders');
 
 export const transactionCollection =
   APP_ENV === 'production'
-    ? firestore().collection('transactions')
+    ? firestore().collection('liveTransactions')
     : firestore().collection('devTransactions');
 
 export const favoritesCollection =
   APP_ENV === 'production'
-    ? firestore().collection('favorites')
+    ? firestore().collection('liveFavorites')
     : firestore().collection('devFavorites');
 
 export const appConstants = firestore().collection('appConstants');
