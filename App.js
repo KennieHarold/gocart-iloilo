3fcbff51b979647fc8a09c66bac4dafc5c907d21
Kennie Harold <kennie.harold@gmail.com>
2021-08-25 21:54:45 +0800

    Add version checker

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {LogBox} from 'react-native';
+import {LogBox, Alert, Linking} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -13,6 +13,40 @@ import {
 } from './src/components/Modals';
 
 class App extends React.Component {
+  componentDidMount() {
+    this.checkUpdatedVersion();
+  }
+
+  checkUpdatedVersion = async () => {
+    const response = await VersionCheck.needUpdate({country: 'us'});
+
+    console.log('Update Needed? ', response);
+
+    const title = 'Please Update';
+    const message =
+      'You will have to update your app to the latest version to continue using.';
+
+    if (response && response.isNeeded) {
+      Alert.alert(
+        title,
+        message,
+        [
+          {
+            text: string.Update,
+            onPress: () => {
+              Linking.openURL(response.storeUrl).finally(() => {
+                RNExitApp.exitApp();
+              });
+            },
+          },
+        ],
+        {
+          cancelable: false,
+        },
+      );
+    }
+  };
+
   render() {
     LogBox.ignoreLogs(['Warning: ...']);
     LogBox.ignoreAllLogs();
